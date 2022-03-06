47009e05ace1f90f0c6a53fd831901cac415a749
Laura-Dancoso <lauradancoso@gmail.com>
2022-03-06 20:42:18 -0300

    async final part

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,4 +1,6 @@
-import { fakeAsync, flush, tick } from "@angular/core/testing";
+import { fakeAsync, flush, flushMicrotasks, tick } from "@angular/core/testing";
+import { of} from "rxjs";
+import { delay } from "rxjs/operators";
 
 fdescribe('Async Testing Examples',()=>{
 
@@ -24,7 +26,7 @@ fdescribe('Async Testing Examples',()=>{
     expect(test).toBeTruthy();
   }));
 
-  it('Asynchronous test example - Plain Promise', ()=>{
+  it('Asynchronous test example - Plain Promise', fakeAsync(()=>{
     let test = false;
 
     console.log('Creating promise');
@@ -33,7 +35,37 @@ fdescribe('Async Testing Examples',()=>{
       console.log('Promise evaluated succesfully');
       test = true;
     });
-
+    console.log('Running test assertions');
+    flushMicrotasks();
     expect(test).toBeTruthy();
-  });
+  }));
+
+  it('Asynchronous test example - Promises + setTimeOut()', fakeAsync(()=>{
+    let counter = 0;
+    Promise.resolve().then(()=>{
+      counter+=10;
+      setTimeout(()=>{
+        counter+=1;
+      }, 1000);
+    });
+    
+    expect(counter).toBe(0);
+    flushMicrotasks();  // triggering the code inside the Promise
+    expect(counter).toBe(10);
+    flush(); // triggering the code inside the setTimeOut()
+    expect(counter).toBe(11);
+  }));
+
+  it('Asynchronous test example - Observables', fakeAsync(()=>{
+    let test = false;
+    console.log('Creating Observable');
+    const test$ = of(test).pipe(delay(1000));
+    test$.subscribe(()=>{
+      test=true;
+    });
+    tick(1000);
+    console.log('Running test assertions');
+    expect(test).toBeTruthy();
+  }));
+
 });
